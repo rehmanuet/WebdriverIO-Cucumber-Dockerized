6325f20dad93833749b711e2e5f22bd710eea513
Abdur Rehman <rehman123123@yahoo.com>
2020-03-18 19:26:23 +0500

    Refactored Config

--- wdio.conf-c.js
+++ wdio.conf-c.js
@@ -1,85 +1,24 @@
 exports.config = {
-    //
-    // ====================
-    // Runner Configuration
-    // ====================
-    //
-    // WebdriverIO allows it to run your tests in arbitrary locations (e.g. locally or
-    // on a remote machine).
     runner: 'local',
-    //
-    // Override default path ('/wd/hub') for chromedriver service.
-    // path: '/',
+    // ======================================== //
+    // ====================
+    // ChromeDriver Runner
+    // ====================
+    services: ['chromedriver'],
+    path: '/',
 
-    //
-    // ==================
-    // Specify Test Files
-    // ==================
-    // Define which test specs should run. The pattern is relative to the directory
-    // from which `wdio` was called. Notice that, if you are calling `wdio` from an
-    // NPM script (see https://docs.npmjs.com/cli/run-script) then the current working
-    // directory is where your package.json resides, so `wdio` will be called from there.
-    //
-    specs: ['./features/**/*.feature'],
-    // Patterns to exclude.
-    exclude: [
-        // 'path/to/excluded/files'
-    ],
-    //
-    // ============
-    // Capabilities
-    // ============
-    // Define your capabilities here. WebdriverIO can run multiple capabilities at the same
-    // time. Depending on the number of capabilities, WebdriverIO launches several test
-    // sessions. Within your capabilities you can overwrite the spec and exclude options in
-    // order to group specific specs to a specific capability.
-    //
-    // First, you can define how many instances should be started at the same time. Let's
-    // say you have 3 different capabilities (Chrome, Firefox, and Safari) and you have
-    // set maxInstances to 1; wdio will spawn 3 processes. Therefore, if you have 10 spec
-    // files and you set maxInstances to 10, all spec files will get tested at the same time
-    // and 30 processes will get spawned. The property handles how many capabilities
-    // from the same test should run tests.
-    //
-    maxInstances: 10,
-    //
-    // If you have trouble getting all important capabilities together, check out the
-    // Sauce Labs platform configurator - a great tool to configure your capabilities:
-    // https://docs.saucelabs.com/reference/platforms-configurator
-    //
-    
-    capabilities: [{
+    // ======================================== //
 
-        maxInstances: 5,
-        //
-        browserName: 'chrome',
- 
-    }],
-    //
-    // ===================
-    // Test Configurations
-    // ===================
-    // Define all options that are relevant for the WebdriverIO instance here
-    //
-    // Level of logging verbosity: trace | debug | info | warn | error | silent
-    logLevel: 'info',
-    outputDir: './logs',
+    // ===========================
+    // Selenium Stand Alone Runner
+    // ==========================
 
+    // services: ['selenium-standalone'],
+    // hostname:'selenium',
+    // path:'/wd/hub',
+    // port:"4444",
+    // ======================================== //
 
-    bail: 0,
-
-    baseUrl: 'http://localhost',    //
-
-    waitforTimeout: 10000,
-
- 
-    connectionRetryTimeout: 90000,
-    connectionRetryCount: 3,
-   
-    services: ['selenium-standalone'],
-    hostname:'selenium',
-    path:'/wd/hub',
-    port:"4444",
     // services: ['docker'],
     // dockerOptions: {
     //     image: 'vvoyer/selenium-standalon',
@@ -89,9 +28,29 @@ exports.config = {
     //         shmSize: '2g'
     //     }
     // },
-   
-    framework: 'cucumber',
 
+
+
+
+    specs: ['./features/**/*.feature'],
+    exclude: [],
+    maxInstances: 10,
+    capabilities: [{
+        maxInstances: 5,
+        browserName: 'chrome',
+    }],
+    // ===================
+    // Test Configurations
+    // ===================
+    // Level of logging verbosity: trace | debug | info | warn | error | silent
+    logLevel: 'info',
+    outputDir: './logs',
+    bail: 0,
+    baseUrl: 'http://localhost:8080',
+    waitforTimeout: 10000,
+    connectionRetryTimeout: 90000,
+    connectionRetryCount: 3,
+    framework: 'cucumber',
     reporters: ['allure', 'spec'],
     reporterOptions: {
         allure: {
@@ -101,7 +60,6 @@ exports.config = {
             useCucumberStepReporter: false
         }
     },
-    //
     cucumberOpts: {
         requireModule: ['@babel/register'],
         require: ['./features/step_definitions/**/*.js'], // <string[]> (file/dir) require files before executing features
@@ -118,14 +76,6 @@ exports.config = {
         timeout: 60000, // <number> timeout for step definitions
         ignoreUndefinedDefinitions: false, // <boolean> Enable this config to treat undefined definitions as warnings.
     },
-    //
-    // =====
-    // Hooks
-    // =====
-    // WebdriverIO provides several hooks you can use to interfere with the test process in order to enhance
-    // it and to build services around it. You can either apply a single function or an array of
-    // methods to it. If one of them returns with a promise, WebdriverIO will wait until that promise got
-    // resolved to continue.
     /**
      * Gets executed once before all workers get launched.
      * @param {Object} config wdio configuration object
